88d92adfec81ff87f147a4300641286a543b3351
Nitram099 <nitrammato@gmail.com>
2024-08-23 23:59:24 +0300

    Assigment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+
+print("Hello,", name + "!")
+print("You are", age, "years old.")
+print("You live in", location + ".")
+
+print("\nNice to meet you!")
